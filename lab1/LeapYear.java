350b502ed2a64091df4dbeeb909794645b938992
chentong <3210100706@zju.edu.cn>
2022-05-26 09:04:59 +0800

    finish lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -29,5 +29,9 @@ public class LeapYear {
             }
         }
     }
+
+    public static boolean isLeapYear(int year){
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    }
 }
 
